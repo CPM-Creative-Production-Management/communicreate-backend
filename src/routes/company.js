734e118ac17d80088725cd4ac593788d9d6030e9
Fabiha Tasneem <1805072@ugrad.cse.buet.ac.bd>
2023-08-14 19:54:06 +0600

    change to company/dues

--- src/routes/company.js
+++ src/routes/company.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const passport = require('passport')
 const { decodeToken } = require('../utils/helper')
 const { Company, Agency } = require('../models/associations')
-const { Payment } = require('../models/associations')
+const { Payment, Estimation, ReqAgency, Request } = require('../models/associations')
 
 // get Company by id
 router.get('/:id(\\d+)', passport.authenticate('jwt', { session: false }), async (req, res) => {
@@ -26,7 +26,7 @@ router.get('/', async (req, res) => {
 })
 
 // post new Company
-router.post('/', passport.authenticate('jwt', {session: false}), async (req, res) => {
+router.post('/', passport.authenticate('jwt', { session: false }), async (req, res) => {
     const company = await Company.create(req.body)
     res.json(company)
 })
@@ -48,7 +48,7 @@ router.put('/:id(\\d+)', passport.authenticate('jwt', { session: false }), async
 router.delete('/:id(\\d+)', passport.authenticate('jwt', { session: false }), async (req, res) => {
     const id = req.params.id
     const message = await Company.destroy({
-        where : {
+        where: {
             id: id
         }
     })
@@ -70,7 +70,7 @@ router.get('/dues', passport.authenticate('jwt', { session: false }), async (req
     // console.log(paymentJson)
     const today = new Date()
     // calculate due amount for each project
-    for(var i = 0; i < paymentJson.length; i++){
+    for (var i = 0; i < paymentJson.length; i++) {
         paymentJson[i].dueAmount = paymentJson[i].total_amount - paymentJson[i].paid_amount
         paymentJson[i].remaining_installments = paymentJson[i].emi_installment_choice - paymentJson[i].installments_completed
 
@@ -80,13 +80,18 @@ router.get('/dues', passport.authenticate('jwt', { session: false }), async (req
         console.log(today)
         console.log(paymentJson[i].updatedAt)
         var days = Math.floor((today - paymentJson[i].updatedAt) / (1000 * 60 * 60 * 24))
-        if(days > 30){
+        if (days > 30) {
             days = days - 30
             paymentJson[i].days = days + " days overdue"
-        } else {    
+        } else {
             days = 30 - days
             paymentJson[i].days = "Next payment due in " + Math.abs(days) + " days"
         }
+
+        const estimation = await Estimation.findByPk(paymentJson[i].EstimationId)
+        const reqAgency = await ReqAgency.findByPk(estimation.ReqAgencyId)
+        const request = await Request.findByPk(reqAgency.RequestId)
+        paymentJson[i].projectName = request.name
     }
 
     const response = {
