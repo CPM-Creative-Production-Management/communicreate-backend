4ebd087b0ab1199eba6e5dd1b70972d979548407
Fabiha Tasneem <1805072@ugrad.cse.buet.ac.bd>
2023-09-06 05:46:14 +0600

    Whole payment module is now Task based.

--- src/routes/payment_new.js
+++ src/routes/payment_new.js
@@ -6,7 +6,7 @@ const { decodeToken } = require('../utils/helper')
 require('dotenv').config({ path: '../../.env' });
 const { Agency } = require('../models/associations')
 const { Company } = require('../models/associations')
-const { Payment, Estimation, ReqAgency, Request } = require('../models/associations')
+const { Payment, Estimation, ReqAgency, Request, Task } = require('../models/associations')
 const { PaymentHistory } = require('../models/associations')
 const bodyParser = require('body-parser').json()
 
@@ -22,23 +22,9 @@ const success_url = baseurl + 'payment/success'
 const fail_url = baseurl + 'payment/failure'
 
 router.post('/new', passport.authenticate('jwt', { session: false }), async (req, res) => {
-    payment_category = ''
-    emi_installment_choice = 0
-    if (req.body.emi_option == 0) {
-        payment_category = 'FULL'
-        emi_installment_choice = 0
-    }
-    else {
-        payment_category = 'EMI'
-        emi_installment_choice = req.body.emi_installment_choice
-    }
     const newPayment = await Payment.create({
         total_amount: req.body.amount,          //SSLCommerz can allow 10.00 BDT to 500000.00 BDT per transaction
         paid_amount: 0,
-        currency: 'BDT',                        //currency = BDT/USD/INR (3 letters fixed)
-        category: payment_category,                        //full_payment or half_payment or emi
-        emi_installment_choice: emi_installment_choice,  //if total = 12, then total interest will be fixed for 12 months
-        installments_completed: 0,
         EstimationId: req.body.estimation_id,                    //estimation_id
         CompanyId: req.body.company_id,                    // customer name = company id
         AgencyId: req.body.agency_id,                      // ship name = agency id
@@ -62,26 +48,38 @@ router.post('/new', passport.authenticate('jwt', { session: false }), async (req
 //sslcommerz initialize payment
 router.post('/:id(\\d+)/init', passport.authenticate('jwt', { session: false }), async (req, res) => {
     const transaction_id = crypto.randomUUID({ disableEntropyCache: true })
-    emi_option = 0
     const payment_id = req.params.id
     const payment = await Payment.findByPk(payment_id)
-    //console.log(payment)
+    console.log(payment)
+
+    const task = await Task.findByPk(req.body.taskId)
+    console.log("Task : ", task)
+    let amount = task.cost
+
+    // count number of isPaid = 0 tasks under this particular estimation
+    const remaining_tasks_count = await Task.count({
+        where: {
+            isPaid: 0,
+            EstimationId: payment.EstimationId
+        }
+    })
+    console.log(remaining_tasks_count)
+    // if only one task is remaining, then pay the rest of the amount as well to complete the whole payment
+    if(remaining_tasks_count == 1){
+        amount = payment.total_amount - payment.paid_amount
+    }
 
     const company = await Company.findByPk(payment.CompanyId)
     const company_email = company.email
     const company_phone = company.phone
 
-    var amount = payment.total_amount;
-    if (payment.category == 'EMI') {
-        emi_option = 1
-    }
-
     //insert into payment_history table
     const newPaymentHistory = await PaymentHistory.create({
         PaymentId: payment_id,
         transaction_id: transaction_id,
-        amount: req.body.amount,
-        status: 'pending'
+        amount: amount,
+        status: 'pending',
+        TaskId: req.body.taskId
     }).catch(function (err) {
         const error = {
             responseCode: 0,
@@ -94,20 +92,20 @@ router.post('/:id(\\d+)/init', passport.authenticate('jwt', { session: false }),
     if (true) {
         //console.log('Payment History Inserted')
         const data = {
-            total_amount: req.body.amount,          //SSLCommerz can allow 10.00 BDT to 500000.00 BDT per transaction
+            total_amount: amount,          //SSLCommerz can allow 10.00 BDT to 500000.00 BDT per transaction
             currency: 'BDT',                        //currency = BDT/USD/INR (3 letters fixed)
             tran_id: transaction_id,                //unique transaction id
             success_url: success_url,
             fail_url: fail_url,
             cancel_url: success_url,
             ipn_url: success_url,       //Instant Payment Notification (IPN) URL of website where SSLCOMMERZ will send the transaction's status
-            shipping_method: 'OnlinePayment',                   // not necessary
-            product_name: payment.EstimationId,               //estimation_id
-            product_category: payment.category,                 //full or emi
-            emi_option: emi_option,                    // 0 for full payment, 1 for emi
-            emi_max_inst_option: 12,                            // Max installments we allow: 3 / 6 / 9 / 12. We are keeping 12 fixed.
-            emi_selected_inst: payment.emi_installment_choice,      // 3 / 6 / 9 / 12
-            emi_allow_only: 0,     // Value is always 0. If 1 then only EMI is possible, no Mobile banking and internet banking channel will not display. 
+            shipping_method: 'Online Payment',                  // not necessary
+            product_name: payment.EstimationId,                 //estimation_id
+            product_category: 'TASK BASED',                     //not necessary
+            emi_option: 0,                                      // 0 for full payment, 1 for emi
+            emi_max_inst_option: 0,                            // Max installments we allow 0
+            emi_selected_inst: 0,                               // 0
+            emi_allow_only: 0,                                  // Value is always 0. If 1 then only EMI is possible, no Mobile banking and internet banking channel will not display. 
             cus_name: payment.CompanyId,                      // customer name = company id
             cus_email: company_email,                           // customer email = company email
             cus_phone: company_phone,                           // customer phone = company phone
@@ -155,10 +153,25 @@ router.get('/:id(\\d+)/history', passport.authenticate('jwt', { session: false }
     })
     var paymentHistoryJson = payment_history.map(p => p.toJSON());
 
-    const estimation = await Estimation.findByPk(paymentJson.EstimationId)
-    const reqAgency = await ReqAgency.findByPk(estimation.ReqAgencyId)
-    const request = await Request.findByPk(reqAgency.RequestId)
-    paymentJson.projectName = request.name
+    const project = await Payment.findByPk(paymentJson.id, {
+        include: [
+            {
+                model: Estimation,
+                include: [
+                    {
+                        model: ReqAgency,
+                        include: [
+                            {
+                                model: Request,
+                                attributes: ['name'],
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    });
+    paymentJson.projectName = project.Estimation.ReqAgency.Request.name;
 
     for (var i = 0; i < paymentHistoryJson.length; i++) {
         const date = { day: '2-digit', month: 'long', year: 'numeric' };
@@ -218,28 +231,32 @@ router.post('/success', async (req, res) => {
 
             // update payment table
             const payment = await Payment.findByPk(updated_payment_history.PaymentId)
-            const amount_per_inst = ((payment.total_amount - payment.paid_amount) / (payment.emi_installment_choice - payment.installments_completed)).toFixed(2)
-            
-            var installments = 0
-            if(payment.category == "EMI"){
-                installments = Math.floor(updated_payment_history.amount / amount_per_inst)
-            }
-            console.log('installments: ', installments)
-
             const updated_payment = await Payment.update({
-                paid_amount: payment.paid_amount + updated_payment_history.amount,
-                installments_completed: payment.installments_completed + installments
+                paid_amount: payment.paid_amount + updated_payment_history.amount
             }, {
                 where: {
                     id: payment.id
                 },
             });
             const updated_payment2 = await Payment.findByPk(payment.id)
-            //console.log(updated_payment2)
+            console.log(updated_payment2)
+
+            // update Task table. make isPaid = 1
+            const task = await Task.findByPk(updated_payment_history.TaskId)
+            const updated_task = await Task.update({
+                isPaid: 1
+            }, {
+                where: {
+                    id: task.id
+                },
+            });
+            const updated_task2 = await Task.findByPk(task.id)
+            console.log(updated_task2)
 
             responseData = {
                 payment_history: updated_payment_history,
                 payment: updated_payment2,
+                task: updated_task2,
                 data: data
             }
             console.log('returning to frontend')
@@ -330,39 +347,46 @@ router.get('/:id(\\d+)/dues', passport.authenticate('jwt', { session: false }), 
     const company = await Company.findByPk(paymentJson.CompanyId)
     paymentJson.companyName = company.name
 
-    paymentJson.dueAmount = (paymentJson.total_amount - paymentJson.paid_amount).toFixed(2)
-    paymentJson.remaining_installments = paymentJson.emi_installment_choice - paymentJson.installments_completed
+    paymentJson.total_amount = paymentJson.total_amount.toFixed(2)
+    paymentJson.paid_amount = paymentJson.paid_amount.toFixed(2)
+    paymentJson.due_amount = (paymentJson.total_amount - paymentJson.paid_amount).toFixed(2)
 
-    const today = new Date()
-    var days = Math.floor((today - paymentJson.updatedAt) / (1000 * 60 * 60 * 24))
 
-    if (days >= 0 && days < 30) {
-        if (paymentJson.paid_amount == 0) {      //not updated once after creating
-            paymentJson.overdue = 1
-            paymentJson.message = days + " days overdue"
-            if (paymentJson.category == "EMI" && paymentJson.remaining_installments > 0) {
-                paymentJson.due_to_pay_now = (paymentJson.dueAmount / paymentJson.remaining_installments).toFixed(2)
-            } else if(paymentJson.category == "FULL") {
-                paymentJson.due_to_pay_now = paymentJson.dueAmount
-            }
-        } else if (paymentJson.dueAmount == 0) {
-            paymentJson.overdue = 2
-            paymentJson.message = "Full Payment Done"
-            paymentJson.due_to_pay_now = 0
+    // Case 1: Due > 0. Tasks incomplete —> overdue = 0
+    // Case 2: Due > 0. Task complete —> overdue = 1
+    // Case 3: Due = 0. Tasks incomplete —> overdue = 2
+    // Case 4: Due = 0. All tasks done —>  overdue = 3
+
+
+    const tasks = await Task.findAll({
+        where: {
+            EstimationId: paymentJson.EstimationId
         }
-        else {
+    })
+    paymentJson.tasks = tasks
+    const unpaid_completed_tasks = tasks.filter(task => (task.status === 2 && task.isPaid === 0));
+    const unpaid_incomplete_tasks = tasks.filter(task => (task.status !== 2 && task.isPaid === 0));
+    const paid_completed_tasks = tasks.filter(task => (task.status === 2 && task.isPaid === 1));
+    const paid_incomplete_tasks = tasks.filter(task => (task.status !== 2 && task.isPaid === 1));
+
+    if(paymentJson.due_amount > 0){
+        if (unpaid_incomplete_tasks.length > 0) {
             paymentJson.overdue = 0
-            paymentJson.message = "Dues cleared for this month"
-            paymentJson.due_to_pay_now = 0
+            paymentJson.message = "Tasks are incomplete. But advance payment can be made."
         }
-    } else {
-        const months = Math.floor(days / 30)
-        paymentJson.overdue = 1
-        paymentJson.message = months + " months overdue"
-        if (paymentJson.category == "EMI" && paymentJson.remaining_installments > 0 && months > 0) {
-            paymentJson.due_to_pay_now = (months * (paymentJson.dueAmount / paymentJson.remaining_installments)).toFixed(2)
-        } else if(paymentJson.category == "FULL") {
-            paymentJson.due_to_pay_now = paymentJson.dueAmount
+        else if (unpaid_completed_tasks.length > 0) {
+            paymentJson.overdue = 1
+            paymentJson.message = "Payment Due for Completed Tasks."
+        }
+    }
+    else if (paymentJson.due_amount == 0) {
+        if (paid_incomplete_tasks.length > 0) {
+            paymentJson.overdue = 2
+            paymentJson.message = "No dues to be cleared. But tasks are still incomplete."
+        }
+        else if (paid_completed_tasks.length > 0) {
+            paymentJson.overdue = 3
+            paymentJson.message = "Full Payment Done."
         }
     }
 
@@ -373,40 +397,5 @@ router.get('/:id(\\d+)/dues', passport.authenticate('jwt', { session: false }), 
     }
     res.json(response)
 })
-
-//calculate total amount for different EMI choices
-// router.put('/:id(\\d+)/calculateEMI', passport.authenticate('jwt', { session: false }), async (req, res) => {
-//     var payment = await Payment.findByPk(req.params.id)        
-//     var paymentJson
-
-//     // get params from body
-//     const N = req.body.emi_installment_choice
-//     const P = req.body.total_amount
-//     const R = req.body.rate /100.0
-//     //console.log("N=",N," P=", P, " R=",R)
-
-//     if(payment.category == "EMI"){
-//         // calculate total_amount using EMI formula PR(1+R)^N/[((1+R)^N)-1]
-//         var amount_per_installment = (P * R * Math.pow((1 + R), N)) / (Math.pow((1 + R), N) - 1)
-//         amount_per_installment = Number(amount_per_installment.toFixed(2))
-//         var total_amount = amount_per_installment * N
-//         total_amount = Number(total_amount.toFixed(2))
-//         //console.log("total_amount=", total_amount, " amount_per_installment=", amount_per_installment)
-
-//         await payment.update({
-//             total_amount: total_amount
-//         })
-
-//         paymentJson = payment.toJSON()
-//         paymentJson.amount_per_installment = amount_per_installment
-//     }
-//     res.json({
-//         responseCode: 1,
-//         responseMessage: 'Success',
-//         responseData: paymentJson
-//     })
-// })
-
-
 
 module.exports = router
