294c408094ee71fdd7e6c787acc3ffb6b53eb926
Showvik Biswas <showvikbiswas@hotmail.com>
2023-09-19 12:23:52 +0600

    websocket notifications

--- express.js
+++ express.js
@@ -2,11 +2,12 @@ const express = require('express')
 const app = express()
 const { createServer } = require('node:http');
 const { Server } = require("socket.io");
+require('dotenv').config()
 
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3002',
+        origin: process.env.FRONTEND_URL,
     }
 });
 
